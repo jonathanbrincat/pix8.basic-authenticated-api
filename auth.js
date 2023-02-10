1fb12f85121d7479f5860a6a7f0701a9e3805363
jonathan <jonathan@pix8.co.uk>
2023-02-10 03:53:38 +0000

    passport and jwt strategy modules; auth class staged

---
+++ auth.js
@@ -0,0 +1,10 @@
+const passport = require('passport')
+const { Strategy, ExtractJwt } = require('passport-jwt')
+const { auth } = require('./config')
+
+class Auth {
+  constructor() {
+  }
+}
+
+module.exports = Auth
